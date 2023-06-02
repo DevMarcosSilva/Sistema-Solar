617fd01a8bae389c2583b6ec56e089dc48591817
marcosgs <mvinicius17112002@gmail.com>
2023-06-02 20:06:08 -0300

    atl

---
+++ main.js
@@ -0,0 +1,6 @@
+function mover(){
+    let sol = document.getElementById("sol");
+    sol.style.transform = 'translateX(50vw)'
+    sol.style.transition = 'all 2s'
+    sol.style.zIndex = '5 !important'
+}
